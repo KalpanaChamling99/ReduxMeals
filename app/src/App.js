1d9d706d089d7612be1477713252b70a1d015acb
kalpanachamling99 <kalpanachamling99@gmail.com>
2022-01-20 15:49:23 +0545

    send cart data to firebase

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import {Fragment} from 'react';
+import {Fragment,useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
 import Header from './Components/Header/Header';
@@ -10,6 +10,26 @@ import './App.css';
 
 function App() {
   const cartIsVisible = useSelector((state)=>state.ui.cartIsVisible);
+  const cart = useSelector((state)=>state.cart);
+
+  useEffect(()=>{
+    const sendCartData = async() =>{
+      const response = await fetch(
+        'https://reduxmeal-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        }
+      );
+      if(!response.ok){
+        throw new Error('something went wrong');
+      }
+    }
+    sendCartData().catch(error=>{
+      
+    });
+    
+  },[cart]);
 
   return (
     <Fragment>
