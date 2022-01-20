de5bd7c09c82d8615f817b74c257e495502759f2
kalpanachamling99 <kalpanachamling99@gmail.com>
2022-01-20 10:01:01 +0545

    added slice for cart button to toggle cart item

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,21 @@
 import {Fragment} from 'react';
+import {useSelector} from 'react-redux';
+
 import Header from './Components/Header/Header';
 import Cart from './Components/Cart/Cart';
 import ProductList from './Components/Shop/Product';
+
+
 import './App.css';
 
 function App() {
+  const cartIsVisible = useSelector((state)=>state.ui.cartIsVisible);
+
   return (
     <Fragment>
       <Header />
       <div className="main-content container">
-        <Cart />
+        {cartIsVisible &&  <Cart />}
         <ProductList />
       </div>
     </Fragment>
--- app/src/Components/Header/Button.js
+++ app/src/Components/Header/Button.js
@@ -1,8 +1,15 @@
 import classes from './header.module.scss';
+import {useDispatch} from 'react-redux';
+import { uiActions } from './../../Store/ui-slice';
+
 
 const CartButton = props => {
+    const dispatch = useDispatch();
+    const toggleCartHandler = () => {
+        dispatch(uiActions.toggle());
+    }
     return(
-        <button className={classes['cart-btn']}>
+        <button className={classes['cart-btn']} onClick={toggleCartHandler}>
             <span>My cart</span>
             <span className={classes.counter}>10</span>
         </button>
